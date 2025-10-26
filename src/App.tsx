a0cfbbf8f7ba614ff1af4d6128521992cb0fbe45
nahidhk <nahidappltd@gmail.com>
2025-10-26 23:49:22 +0600

    add the fast json file and with revotin

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import databasesImg from './assets/json.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="flex center medel fill dark">
+    <div className="flex center medel row">
+      <img className='logo' src={databasesImg}  />
+      <h1>
+        ICX JSON API SERVER
+      </h1>
+      <code>
+        {window.location.href}api/data/[yourJsonFile].json
+      </code>
     </div>
+   </div>
   );
 }
 
